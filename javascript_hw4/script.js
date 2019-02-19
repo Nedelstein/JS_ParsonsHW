2e372d53747f77dc8cce06bb35a543bf625aca69
Noah Edelstein <Noedelstein@gmail.com>
2019-02-19 14:22:22 -0500

    week 4 hw

--- javascript_hw4/script.js
+++ javascript_hw4/script.js
@@ -1,5 +1,16 @@
 
-const bot ={
+// const bot ={
+
+// 	name: "You can call me Mr. Chat Bot",
+// 	home: "The Internet is my home",
+// 	madeOf: "I am made of 1s and 0s",
+// 	food: "I eat your data",
+// 	age: "I am ageless",
+// };
+
+function answerQuestion(question){
+
+	const bot ={
 
 	name: "You can call me Mr. Chat Bot",
 	home: "The Internet is my home",
@@ -8,16 +19,16 @@ const bot ={
 	age: "I am ageless",
 };
 
-function answerQuestion(question){
-	if (question == "What is your name?" || question.includes('name')){
+
+	if (question.toLowerCase().includes('name') || question.toLowerCase().includes('called') || question.toLowerCase().includes('who are you')){
 		console.log(bot.name);
-	} else if (question == "Where are you from?" || question.includes('home') || question.includes('from')){
+	} else if (question.toLowerCase().includes('home') || question.toLowerCase().includes('from')){
 		console.log(bot.home);
-	} else if (question == "What are you made of?" || question.includes('made')){
+	} else if (question.toLowerCase().includes('made') || question.toLowerCase().includes('built of')){
 		console.log(bot.madeOf);
-	} else if (question == "What do you eat?" || question.includes('eat') || question.includes('food')){
+	} else if (question.toLowerCase().includes('food') || question.toLowerCase().includes('you eat') || question.toLowerCase().includes('eat')){
 		console.log(bot.food);
-	} else if (question == "How old are you?" || question.includes('age') || question.includes('old')){
+	} else if (question.toLowerCase().includes('age') || question.toLowerCase().includes('old') || question.toLowerCase().includes('been around')){
 		console.log(bot.age);
 	}
 	else {
