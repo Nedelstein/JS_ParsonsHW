338d6591e6b4483808a8790f80bb8e8cde1dc5e8
Noah Edelstein <Noedelstein@gmail.com>
2019-03-20 13:13:53 -0400

    added sound, ingredients, servings, original recipe url

--- midterm/script.js
+++ midterm/script.js
@@ -1,21 +1,24 @@
 const edamamApi = "https://api.edamam.com/search?";
 const api_id = "&app_id=8aed0130";
 const api_key = "&app_key=10bf9207b3dc2e5353697f16d6db3510";
-const result_count = "&from=0&to=1";
+const result_count = "&from=0&to=8";
 let url;
 const vegetarianHealthLabel = "&health=vegetarian";
 const veganHealthLabel = "&health=vegan";
 const balancedHealthLabel = "&diet=balanced";
 
 window.onload = function() {
+  document.getElementById("audio").play();
+
   let submitted = document.getElementById("typeIngredients");
   let vegetarianValue = document.getElementById("vegetarian");
   let veganValue = document.getElementById("vegan");
   let balancedValue = document.getElementById("balanced");
+  let recipeUrl = document.getElementById("recipeLink");
 
   submitted.onsubmit = function(e) {
     e.preventDefault();
-
+    let randArrayItem = Math.floor(Math.random() * 8);
     let ingredient = "q=" + submitted.item.value;
     if (veganValue.checked) {
       console.log("vegan");
@@ -69,17 +72,19 @@ window.onload = function() {
       .then(response => response.json())
       .then(json => {
         console.log(json);
-        console.log(json.hits[0].recipe.label);
-        console.log(json.hits[0].recipe.ingredientLines);
-        console.log(json.hits[0].recipe.healthLabels);
-        console.log(json.hits[0].recipe.dietLabels);
+        console.log(randArrayItem);
 
         //print recipe title
         document.getElementById("recipeTitle").innerHTML =
-          json.hits[0].recipe.label;
+          json.hits[randArrayItem].recipe.label;
+
+        //print servings number
+        document.getElementById("servings").innerHTML =
+          "Servings: " + json.hits[randArrayItem].recipe.yield;
+        document.getElementById("servings").style.display = "block";
 
         //print out recipe line by line
-        let recipeList = json.hits[0].recipe.ingredientLines;
+        let recipeList = json.hits[randArrayItem].recipe.ingredientLines;
         let listItem = "";
 
         for (let i = 0; i < recipeList.length; i++) {
@@ -87,11 +92,18 @@ window.onload = function() {
           listItem += listNumber + " " + recipeList[i] + "<br><br>";
         }
         document.getElementById("recipe").innerHTML = listItem;
-        // break;
+        document.getElementById("ingredients").style.display = "block";
 
         //get food image for each recipe
         document.getElementById("recipeImg").style.display = "block";
-        document.getElementById("recipeImg").src = json.hits[0].recipe.image;
+        document.getElementById("recipeImg").src =
+          json.hits[randArrayItem].recipe.image;
+
+        //link to original recipe
+        recipeUrl.style.display = "block";
+        recipeUrl.onclick = function() {
+          window.open(json.hits[randArrayItem].recipe.url);
+        };
       })
       .catch(err => {
         console.log(err);
